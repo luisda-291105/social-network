a1a159bc7741de8ba521a680b90cdd94d04adeeb
luisDaniel <estudiosluisperez@gmail.com>
2026-03-19 23:17:59 -0500

    feat : se creo y se completo el componente del perfil y se agregaron tag <br /> para separar cada componente

--- src/components/left-column/SideBarLeft.jsx
+++ src/components/left-column/SideBarLeft.jsx
@@ -1,14 +1,18 @@
-import Profile from "./Profile"
-import Accordion from "./Accordion"
-import Interests from "./Interests"
-import AlertBox from "./AlertBox"
-
+import Profile from "./Profile";
+import Accordion from "./Accordion";
+import Interests from "./Interests";
+import AlertBox from "./AlertBox";
 
 export default function SideBarLeft() {
-    return  <>
+    return (
+        <div class="w3-col m3">
             <Profile />
+            <br />
             <Accordion />
+            <br />
             <Interests />
+            <br />
             <AlertBox />
-        </>;
+        </div>
+    );
 }
