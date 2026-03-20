c94e98a77facd47a05ef615b33898f08e505ded1
luisDaniel <estudiosluisperez@gmail.com>
2026-03-20 00:14:10 -0500

    feat : se implememto los componentes creados anterior mente creados , solo se le coloco una pocicion pero no esta funcional

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import SideBarLeft from "./components/left-column/SideBarLeft";
-
+import Middle from "./components/middle-column/Middle";
 function App() {
-    return <>
-        <SideBarLeft />
-    </>;
+    return (
+        <>
+            <div
+                class="w3-container w3-content"
+                style={{maxWidth:"1400px" , marginTop:"80px"}}
+            >
+                <SideBarLeft />
+                <Middle />
+            </div>
+        </>
+    );
 }
 
 export default App;
---
+++ src/components/middle-column/Middle.jsx
@@ -0,0 +1,11 @@
+import CreatePosh from "./CreatePosh";
+import Posh from "./Posh";
+
+export default function Middle() {
+    return (
+        <div class="w3-col m7">
+            <CreatePosh />
+            <Posh />
+        </div>
+    );
+}
