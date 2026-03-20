71447f2f901102b5fde56dc0234274ed845f0c48
luisDaniel <estudiosluisperez@gmail.com>
2026-03-20 00:42:01 -0500

    feat : se creo el componente de la barra de navegacion y se implemento , no tiene logica

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-
+import NavBar from "./components/header/NavBar";
 import Left from "./components/left-column/Left";
 import Middle from "./components/middle-column/Middle";
 import Right from "./components/right-column/Right";
@@ -7,11 +7,12 @@ import Footer from "./components/Footer";
 function App() {
     return (
         <>
-            <div class="w3-container w3-content" style={{maxWidth:"1400px" , marginTop:"80px"}}>
-                <Left />
-                <Middle />
-                <Right />
-            </div>
+        <NavBar />
+        <div class="w3-container w3-content" style={{maxWidth:"1400px" , marginTop:"80px"}}>
+            <Left />
+            <Middle />
+            <Right />
+        </div>
         <Footer />
         </>
     );
