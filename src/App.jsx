4fd2b81e696a6acf5347bbacb4d346247a6de681
luisDaniel <estudiosluisperez@gmail.com>
2026-03-20 00:36:03 -0500

    feat : se creo y se implemento el footer , solo tiene componentes con texto no tiene link

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,17 @@
 import Left from "./components/left-column/Left";
 import Middle from "./components/middle-column/Middle";
 import Right from "./components/right-column/Right";
-
+import Footer from "./components/Footer";
 
 function App() {
     return (
         <>
-            <div
-                class="w3-container w3-content"
-                style={{maxWidth:"1400px" , marginTop:"80px"}}
-            >
+            <div class="w3-container w3-content" style={{maxWidth:"1400px" , marginTop:"80px"}}>
                 <Left />
                 <Middle />
                 <Right />
             </div>
+        <Footer />
         </>
     );
 }
