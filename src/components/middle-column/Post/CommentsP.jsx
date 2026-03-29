44c649f344b246bf6c59364b06b145de29e9f5d7
luisDaniel <estudiosluisperez@gmail.com>
2026-03-29 17:30:49 -0500

    style : se le aplico identacion para mallor entendimiento

--- src/components/middle-column/Post/CommentsP.jsx
+++ src/components/middle-column/Post/CommentsP.jsx
@@ -1,11 +1,14 @@
-import {useContext} from 'react'
-import {ContextComments} from './GroupBtn'
+import { useContext } from "react";
+import { ContextComments } from "./GroupBtn";
 
 export default function CommentsP() {
-    let [comment , setComments] = useContext(ContextComments)
-  return (
-    <div >
-        <p>{comment}</p>
-    </div>
-  )
+    let [comment, setComments] = useContext(ContextComments);
+
+
+    useEffect(submit , [])
+    return (
+        <div>
+            <p>{comment}</p>
+        </div>
+    );
 }
