8521248eecdf1ef7932d55cbe78684f9cb38dcbd
luisDaniel <estudiosluisperez@gmail.com>
2026-03-29 16:51:58 -0500

    feat : se creo las interacciones de los like usando manejo de estados y un contador provicional

--- src/components/middle-column/Post/BtnLike.jsx
+++ src/components/middle-column/Post/BtnLike.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+import {useState} from "react";
 
 export default function BtnLike() {
+    let [counter , setCounter] = useState(0)
     return (
         <button
             type="button"
             className="w3-button w3-theme-d1 w3-margin-bottom"
+            onClick={() => setCounter(counter +1)}
         >
-            <i className="fa fa-thumbs-up"></i>  Like
+            <i className="fa fa-thumbs-up"></i> {counter === 0 ?" Like" :  counter}
         </button>
     );
 }
