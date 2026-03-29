7dde55f1ba21e459ff94044e8bd872222812ed71
luisDaniel <estudiosluisperez@gmail.com>
2026-03-29 17:04:41 -0500

    feat : se creo la interaccion de los comentarios

--- src/components/middle-column/Post/BtnComment.jsx
+++ src/components/middle-column/Post/BtnComment.jsx
@@ -1,12 +1,24 @@
-import React from "react";
+import {useState} from "react";
 
 export default function BtnComment() {
+    let [inputComment , setInputComment]  = useState(false) 
+    let [comments , setComments] = useState("")
+
+    function contentComment(event) {
+        setComments(event.target.value)
+    }
+
     return (
+        <>
         <button
                 type="button"
                 className="w3-button w3-theme-d2 w3-margin-bottom"
+                onClick={() => setInputComment(!inputComment)}
             >
                 <i className="fa fa-comment"></i>  Comment
             </button>
+            {inputComment ? <input type="text" placeholder="Comment here" onChange={contentComment} /> : ""}
+            <p>{comments}</p>
+            </>
     );
 }
