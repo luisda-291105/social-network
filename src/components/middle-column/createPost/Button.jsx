cece19d124f9b4692cbe11a4901e260d772589a7
luisDaniel <estudiosluisperez@gmail.com>
2026-04-02 20:31:04 -0500

    delete : se elimino para reacerlo mas simple

--- src/components/middle-column/createPost/Button.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Button() {
-    return (
-        <button type="button" className="w3-button w3-theme">
-            <i className="fa fa-pencil"></i>  Post
-        </button>
-    );
-}
--- src/components/middle-column/createPost/TextH6.jsx
+++
@@ -1,7 +0,0 @@
-import { useContext } from "react";
-import { ContectText } from "../CreatePosh";
-
-export default function TextH6() {
-    let [Text ] = useContext(ContectText)
-    return <h6 className="w3-opacity">{Text}</h6>;
-}
