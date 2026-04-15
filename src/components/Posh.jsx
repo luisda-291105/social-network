5f7fb432714c3868ab3ff758eb8945982245e625
luisDaniel <estudiosluisperez@gmail.com>
2026-04-14 23:57:28 -0500

    feat : se agrego una lista y se esta mapeando , para poder tener mas publicaciones

--- src/components/Posh.jsx
+++ src/components/Posh.jsx
@@ -3,13 +3,71 @@ import ComentForm from "./commentForm";
 import ListComment from "./listComments";
 
 export default function Posh() {
+    const posh_nuevos = [
+        {
+            id: 1,
+            imagen: "https://picsum.photos/300?random=1",
+            descripcion:
+                "Camiseta oversize color negro, estilo urbano moderno.",
+        },
+        {
+            id: 2,
+            imagen: "https://picsum.photos/300?random=2",
+            descripcion: "Pantalón cargo beige con múltiples bolsillos.",
+        },
+        {
+            id: 3,
+            imagen: "https://picsum.photos/300?random=3",
+            descripcion: "Chaqueta denim clásica azul, corte regular.",
+        },
+        {
+            id: 4,
+            imagen: "https://picsum.photos/300?random=4",
+            descripcion: "Hoodie gris con estampado minimalista.",
+        },
+        {
+            id: 5,
+            imagen: "https://picsum.photos/300?random=5",
+            descripcion: "Tenis blancos casuales para uso diario.",
+        },
+        {
+            id: 6,
+            imagen: "https://picsum.photos/300?random=6",
+            descripcion: "Gorra negra ajustable estilo streetwear.",
+        },
+        {
+            id: 7,
+            imagen: "https://picsum.photos/300?random=7",
+            descripcion: "Camisa manga corta con estampado tropical.",
+        },
+        {
+            id: 8,
+            imagen: "https://picsum.photos/300?random=8",
+            descripcion: "Short deportivo ligero color azul oscuro.",
+        },
+        {
+            id: 9,
+            imagen: "https://picsum.photos/300?random=9",
+            descripcion: "Suéter tejido color crema, estilo elegante.",
+        },
+        {
+            id: 10,
+            imagen: "https://picsum.photos/300?random=10",
+            descripcion: "Chaqueta bomber negra con cierre frontal.",
+        },
+        {
+            id: 11,
+            imagen: "https://imgs.search.brave.com/Iw6FYICfb9udkU08V5lzNPi6gJK81zZlMIs5_68ZyS0/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly93YWxs/cGFwZXJzLmNvbS9p/bWFnZXMvaGQvY29v/bGVzdC1uYXJ1dG8t/d2l0aC1nbG93aW5n/LWxpZ2h0LXRpeWJq/eDN1MjdtMGo0MXQu/anBn",
+            descripcion: "hola bb ",
+        },
+    ];
+
     let [like, setLike] = useState(0);
     // let storageLike = [...like]
     let countLikeStorage = () => {
-        setLike(like + 1)
-        localStorage.setItem("countLike" , JSON.stringify(like))
-
-    }
+        setLike(like + 1);
+        localStorage.setItem("countLike", JSON.stringify(like));
+    };
 
     let listCom = [
         /* {
@@ -45,8 +103,8 @@ export default function Posh() {
     }
 
     useEffect(() => {
-        listData = JSON.parse(localStorage.getItem("comentarios"))
-    },[])
+        listData = JSON.parse(localStorage.getItem("comentarios"));
+    }, []);
 
     useEffect(() => {
         {
@@ -59,82 +117,101 @@ export default function Posh() {
     }, [txtComment]);
 
     function compartir() {
-        alert("Aun no esta disponible esta funcionalidad...")
+        alert("Aun no esta disponible esta funcionalidad...");
     }
 
-
     return (
-        <div className="card m-auto my-4" style={{ width: "25rem" }}>
-            <div className="m-2 d-flex ">
-                <img
-                    src="https://www.w3schools.com/w3images/avatar2.png"
-                    alt="Avatar"
-                    className=" w3-left w3-circle w3-margin-right"
-                    style={{ width: "35px" }}
-                />
-                <div className="d-flex flex-column ">
-                    <span style={{ fontSize: "15px" }}>John Doe</span>
-                    <span style={{ fontSize: "10px" }} className="w3-opacity">
-                        1 min
-                    </span>
-                </div>
-            
-                <div className="d-flex ms-auto gap-2">
-                    <i className="bi bi-three-dots fs-4"></i>
-                    <i className="bi bi-x fs-4"></i>
-                </div>
-            </div>
-
-            <div className="card-body">
-                <p className="card-text">hola bb </p>
-            </div>
-
-            <div className="card-body">
-                <img
-                    src="https://imgs.search.brave.com/Iw6FYICfb9udkU08V5lzNPi6gJK81zZlMIs5_68ZyS0/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly93YWxs/cGFwZXJzLmNvbS9p/bWFnZXMvaGQvY29v/bGVzdC1uYXJ1dG8t/d2l0aC1nbG93aW5n/LWxpZ2h0LXRpeWJq/eDN1MjdtMGo0MXQu/anBn"
-                    className="card-img-top"
-                    alt="imagen..."
-                />
-            </div>
-
-            <ul className="list-group list-group-flush">
-                <li className="list-group-item d-flex justify-content-around">
-                    <span className="d-flex gap-1 me-auto">
-                        <i className="bi bi-hand-thumbs-up"></i>
-                        <i className="bi bi-emoji-smile"></i>
-                        <i className="bi bi-heart"></i> {JSON.parse(localStorage.getItem("countLike"))}</span>
-                    <span className=" d-flex gap-2">{storage.length} <i className="bi bi-chat-dots"></i></span>
-                </li>
-                <li className="list-group-item d-flex justify-content-around">
-                    <button
-                        className="btn "
-                        onClick={countLikeStorage}
-                    >
-                        <i className="bi bi-hand-thumbs-up"></i>
+        <>
+            <div className="w-full h-auto ">
+                {posh_nuevos.map((p) => (
+                    <div
+                        key={p.id}
+                        className="card mx-2 my-4 w-auto"
                         
-                        <span className="ps-2">like</span>
-                    </button>
-                    <button
-                        className="btn "
-                        onClick={() => setBtnComment(!btnComment)}
                     >
-                        <i className="bi bi-chat-dots"></i>
-                        
-                        <span className="ps-2">comment</span>
-                    </button>
-                    <button
-                        className="btn"
-                        onClick={compartir}
-                    >
-                        <i className="bi bi-share"></i>
-                        <span className="ps-2">share</span>
-                    </button>
-                </li>
-            </ul>
+                        <div className="m-2 d-flex ">
+                            <img
+                                src="https://www.w3schools.com/w3images/avatar2.png"
+                                alt="Avatar"
+                                className=" w3-left w3-circle w3-margin-right"
+                                style={{ width: "35px" }}
+                            />
+                            <div className="d-flex flex-column ">
+                                <span style={{ fontSize: "15px" }}>
+                                    John Doe
+                                </span>
+                                <span
+                                    style={{ fontSize: "10px" }}
+                                    className="w3-opacity"
+                                >
+                                    1 min
+                                </span>
+                            </div>
 
-            {btnComment && <ComentForm getCommenData={getCommenData} /> }
-            {btnComment && <ListComment  storageData={storage} /> }
-            
-        </div>
+                            <div className="d-flex ms-auto gap-2">
+                                <i className="bi bi-three-dots fs-4"></i>
+                                <i className="bi bi-x fs-4"></i>
+                            </div>
+                        </div>
+
+                        <div className="card-body">
+                            <p className="card-text">{p.descripcion}</p>
+                        </div>
+
+                        <div className="card-body">
+                            <img
+                                src={p.imagen}
+                                className="card-img-top"
+                                alt="imagen..."
+                            />
+                        </div>
+
+                        <ul className="list-group list-group-flush">
+                            <li className="list-group-item d-flex justify-content-around">
+                                <span className="d-flex gap-1 me-auto">
+                                    <i className="bi bi-hand-thumbs-up"></i>
+                                    <i className="bi bi-emoji-smile"></i>
+                                    <i className="bi bi-heart"></i>{" "}
+                                    {JSON.parse(
+                                        localStorage.getItem("countLike"),
+                                    )}
+                                </span>
+                                <span className=" d-flex gap-2">
+                                    {storage.length}{" "}
+                                    <i className="bi bi-chat-dots"></i>
+                                </span>
+                            </li>
+                            <li className="list-group-item d-flex justify-content-around">
+                                <button
+                                    className="btn "
+                                    onClick={countLikeStorage}
+                                >
+                                    <i className="bi bi-hand-thumbs-up"></i>
+
+                                    <span className="ps-2">like</span>
+                                </button>
+                                <button
+                                    className="btn "
+                                    onClick={() => setBtnComment(!btnComment)}
+                                >
+                                    <i className="bi bi-chat-dots"></i>
+
+                                    <span className="ps-2">comment</span>
+                                </button>
+                                <button className="btn" onClick={compartir}>
+                                    <i className="bi bi-share"></i>
+                                    <span className="ps-2">share</span>
+                                </button>
+                            </li>
+                        </ul>
+
+                        {btnComment && (
+                            <ComentForm getCommenData={getCommenData} />
+                        )}
+                        {btnComment && <ListComment storageData={storage} />}
+                    </div>
+                ))}
+            </div>
+        </>
     );
 }
